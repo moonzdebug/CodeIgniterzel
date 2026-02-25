990038297798fa7e32a926f2e5a1587d91f6448e
moonzdebug <jeonhezelmae@gmail.com>
2026-02-25 23:18:14 +0800

    PRACTICE

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,30 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Auth::index');
+$routes->get('/login', 'Auth::index');
+$routes->post('/auth', 'Auth::auth');
+$routes->get('/dashboard', 'Dashboard::index');
+$routes->get('/logout', 'Auth::logout');
+
+// User Acounts routes
+
+$routes->get('/users', 'Users::index');
+$routes->post('users/save', 'Users::save');
+$routes->get('users/edit/(:segment)', 'Users::edit/$1');
+$routes->post('users/update', 'Users::update');
+$routes->delete('users/delete/(:num)', 'Users::delete/$1');
+$routes->post('users/fetchRecords', 'Users::fetchRecords');
+
+
+// Profiling routes
+$routes->get('/profiling', 'Profiling::index');
+$routes->post('profiling/save', 'Profiling::save');
+$routes->get('profiling/edit/(:segment)', 'Profiling::edit/$1');
+$routes->post('profiling/update', 'Profiling::update');
+$routes->delete('profiling/delete/(:num)', 'Profiling::delete/$1');
+$routes->post('profiling/fetchRecords', 'Profiling::fetchRecords');
+
+// Logs routes for admin
+$routes->get('/log', 'Logs::log');
